pvginkel/Tweakkit-Server

Allow Disabling of 1.6.3 Structure Saving

src/main/java/net/minecraft/server/StructureGenerator.java
@@ -178,7 +178,15 @@ public abstract class StructureGenerator extends WorldGenBase {
 
     private void a(World world) {
         if (this.e == null) {
+            // Spigot Start
+            if ( world.spigotConfig.saveStructureInfo )
+            {
             this.e = (PersistentStructure) world.a(PersistentStructure.class, this.a());
+            } else
+            {
+                this.e = new PersistentStructure( this.a() );
+            }
+            // Spigot End
             if (this.e == null) {
                 this.e = new PersistentStructure(this.a());
                 world.a(this.a(), (PersistentBase) this.e);

copypastel/spigot-craftbukkit

src/main/java/net/minecraft/server/StructureGenerator.java
@@ -139,7 +139,13 @@ public abstract class StructureGenerator extends WorldGenBase {
 
     protected void a(World world) {
         if (this.a == null) {
-            this.a = (PersistentStructure) world.a(PersistentStructure.class, this.a());
+            // Spigot Start
+            if (world.spigotConfig.saveStructureInfo) {
+                this.a = (PersistentStructure) world.a(PersistentStructure.class, this.a());
+            } else {
+                this.a = new PersistentStructure(this.a());
+            }
+            // Spigot End
             if (this.a == null) {
                 this.a = new PersistentStructure(this.a());
                 world.a(this.a(), (PersistentBase) this.a);